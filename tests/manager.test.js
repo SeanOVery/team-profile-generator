595edf90fbd1298cdcea5789ca2df6ba072995bb
SeanOVeryMac <seanovery1@gmail.com>
2021-10-29 13:21:13 -0400

    manager class and tests set up

---
+++ tests/manager.test.js
@@ -0,0 +1,31 @@
+const Manager = require('../lib/manager')
+
+describe('Manager tests', () => {
+  test('Expect new Manager to have a name, id, and email and office number', () => {
+    const person = new Manager('Bob', 12, 'email@email.com', 214)
+    expect(person).toHaveProperty('name')
+    expect(person).toHaveProperty('id')
+    expect(person).toHaveProperty('email')
+    expect(person).toHaveProperty('officeNumber')
+    expect(person.name).toBe('Bob')
+    expect(person.id).toBe(12)
+    expect(person.email).toBe('email@email.com')
+    expect(person.officeNumber).toBe(214)
+  })
+  test('Expect getName to return the Manager name', () => {
+    const person = new Manager('Bob', 12, 'email@email.com')
+    expect(person.getName()).toBe('Bob')
+  })
+  test('Expect getId to return the Manager id', () => {
+    const person = new Manager('Bob', 12, 'email@email.com')
+    expect(person.getId()).toBe(12)
+  })
+  test('Expect getEmail to return the Manager email', () => {
+    const person = new Manager('Bob', 12, 'email@email.com')
+    expect(person.getEmail()).toBe('email@email.com')
+  })
+  test('Expect getRole to return the Manager role', () => {
+    const person = new Manager('Bob', 12, 'email@email.com')
+    expect(person.getRole()).toBe('Manager')
+  })
+})
